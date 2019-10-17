9f94674f38890bcfb63248675a059512de992c36
vvenomer <vvenomer@gmail.com>
2019-10-17 23:31:33 +0200

    started following instuction

--- JPNET_02/Program.cs
+++ JPNET_02/Program.cs
@@ -3,19 +3,72 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Unity;
+using Unity.Lifetime;
+using Unity.Injection;
+using Microsoft.Practices.Unity.Configuration;
 
 namespace JPNET_02
 {
     class Program
     {
+        interface ILogger
+        {
+            void Log(string s);
+        }
+        class Logger : ILogger
+        {
+            public void Log(string s) { Console.WriteLine("logger: " + s); }
+        }
+
+        class Worker
+        {
+            public Worker(ILogger log) { m_log = log; }
+            public void Work()
+            {
+                m_log.Log("begin");
+                m_log.Log("end");
+            }
+            private ILogger m_log;
+        }
+
+        class NumLogger : ILogger
+        {
+            public NumLogger(int i = 0) { id = i; }
+            public void Log(string s) { Console.WriteLine("logger(" + id++ + "): " + s); }
+            private int id;
+        }
+
+        class MetWorker
+        {
+            public void Work()
+            {
+                m_log.Log("begin");
+                m_log.Log("end");
+            }
+            public ILogger m_log { get; set; }
+        }
+
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            IUnityContainer container = new UnityContainer();
+            container.RegisterType<ILogger, Logger>();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            var w = container.Resolve<Worker>();
+            w.Work();
+
+            //container.LoadConfiguration();
+
+            container.RegisterType<ILogger, NumLogger>(
+                "numlog",
+                new ContainerControlledLifetimeManager(),
+                new InjectionConstructor(1)
+                );
+
+            var w2 = container.Resolve<MetWorker>("numwork");
+            w2.Work();
+
+            Console.ReadKey();
         }
     }
 }
